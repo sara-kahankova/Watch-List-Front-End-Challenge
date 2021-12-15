47d4a5c766250727b3d69029029abf79e1f4e18c
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2021-12-14 21:04:21 -0600

    models green

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
+end
---
+++ db/migrate/20211215023027_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :overview
+      t.string :poster_url
+
+      t.timestamps
+    end
+  end
+end
