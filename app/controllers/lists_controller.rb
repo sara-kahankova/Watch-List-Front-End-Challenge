e273cf0ea5503ba8d180535221dddc1f4bd2616a
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2021-12-16 22:13:51 -0600

    List features implemented

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    # @list.bookmarks = Bookmark.find(params[list_id])
   end
 
   def new
@@ -12,12 +13,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
+    @list = List.create(list_params)
+    redirect_to list_path(@list)
   end
 
   private
 
   def list_params
-    params.require(@list).permit(:name)
+    params.require(:list).permit(:name)
   end
 end
