4af7ad6aa3e1dedbf33ffabe6d1473a93a1e7f2e
Sara Kahankova <75204851+kahinek1999@users.noreply.github.com>
2021-12-16 21:54:27 -0600

    list index done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,23 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+  end
+
+  private
+
+  def list_params
+    params.require(@list).permit(:name)
+  end
+end
